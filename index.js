483d622a20b61547a39b402849c3c7f0ef145d77
Xander Jake de los Santos <xanderjakeq@gmail.com>
2019-03-22 13:55:43 -0400

    Created Documentation
    routesDocs.md

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
 require('dotenv').config()
 
 const express = require('express')
+const helmet = require('helmet')
+const cors = require('cors')
+const path = require('path')
 
 const projectsRoutes = require('./routes/projectsRoutes')
 const actionsRoutes = require('./routes/actionsRoutes')
 
 const app = express()
 
-app.use(express.json())
+app.use(helmet())
+app.use(cors())
 
+app.use(express.json())
 app.use('/api/projects', projectsRoutes)
 app.use('/api/actions', actionsRoutes)
 
+app.set('view engine', 'html')
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname + '/routesDocs.md'))
+})
 
 const port = process.env.PORT || 4000
 
