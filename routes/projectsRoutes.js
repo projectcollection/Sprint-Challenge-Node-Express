222eda53ef1b3fdfa89968ec202b1563b368ad9d
Xander Jake de los Santos <xanderjakeq@gmail.com>
2019-03-22 12:33:18 -0400

    update and delete routes done.

--- routes/projectsRoutes.js
+++ routes/projectsRoutes.js
@@ -4,12 +4,17 @@ const projectsDb = require('../data/helpers/projectModel')
 
 const router = express.Router()
 
+const message = {
+    serverError: 'internal server error',
+    notFound: 'not found'
+}
+
 router.get('/', async (req, res) => {
     try{
         let projects = await projectsDb.get()
         res.status(200).json(projects)
     }catch(err){
-        res.status(500).json({message: 'internal server error'})
+        res.status(500).json({message: message.serverError})
     }
 })
 
@@ -18,7 +23,7 @@ router.get('/:id', async (req, res) => {
         let project = await projectsDb.get(req.params.id)
         res.status(200).json(project)
     }catch(err){
-        res.status(404).json({message: 'project not found'})
+        res.status(404).json({message: message.notFound})
     }
 })
 
@@ -28,7 +33,30 @@ router.post('/', async (req, res) => {
         let newProject = await projectsDb.insert(req.body)
         res.status(201).json(newProject)
     }catch(err){
-        res.status(500).json({message: 'try again later'})
+        res.status(500).json({message: message.serverError})
+    }
+})
+
+// add middleware to validate
+router.put('/:id', async (req, res) => {
+    try{
+        let project = await projectsDb.update(req.params.id, req.body)
+        res.status(200).json(project)
+    }catch(err){
+        res.status(500).json({message: message.serverError})
+    }
+})
+
+router.delete('/:id', async (req, res) => {
+    try{
+        let count = await projectsDb.remove(req.params.id)
+        if(count > 0){
+            res.status(200).json(count)
+            return
+        }
+        res.status(404).json({message: message.notFound})
+    }catch(err){
+        res.status(500).json({message: message.serverError})
     }
 })
 
